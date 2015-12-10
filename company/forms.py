a80dc897a9080a2b844756f93e59bda596a917b0
itAtelier <nikita@itatelier.ru>
2015-12-10 14:01:11 +0300

    Small form works

--- company/forms.py
+++ company/forms.py
@@ -13,7 +13,7 @@ class CompanyEditForm(ModelForm):
     name = CharField(label="Наименование", required=True, help_text="Краткое наименование клента, без организационной формы")
     org_type = ModelChoiceFieldNameLabel(queryset=CompanyOrgTypes.objects.all(), label_field='val', empty_label=None, initial=3)
     description = CharField(label="Описание компании", required=False, widget=forms.Textarea(attrs={'cols': 60, 'rows':2}))
-    www = CharField(label="WEB сайт", required=False, widget=forms.TextInput(attrs={'size': 50}))
+    # www = CharField(label="WEB сайт", required=False, widget=forms.TextInput(attrs={'size': 50}))
 
     class Meta:
         model = Companies
