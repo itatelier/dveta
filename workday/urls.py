7789681f212ad56772c30a1e9073af52bad45718
itatelier <nikita@itatelier.ru>
2016-10-17 20:34:44 +0300

    menu fix

--- workday/urls.py
+++ workday/urls.py
@@ -3,7 +3,9 @@
 from django.conf.urls import url, include
 from views import *
 from django.contrib.auth import views as auth_views
+import debug_toolbar
 # from phones.views import *
+from django.conf import settings
 
 
 # REST
@@ -22,5 +24,10 @@ urlpatterns = [
     url(r'^(?P<date>\d{1,2}-\d{1,2}-\d{2})/car/(?P<car_pk>\d+)/dds/$', WorkdayDdsView.as_view(), name='workday_dds'),
     url(r'^(?P<date>\d{1,2}-\d{1,2}-\d{2})/car/(?P<car_pk>\d+)/refuels/$', WorkdayRefuelsView.as_view(), name='workday_refuels'),
     url(r'^(?P<date>\d{1,2}-\d{1,2}-\d{2})/car/(?P<car_pk>\d+)/talons/$', WorkdayTalonsView.as_view(), name='workday_talons'),
+    url(r'^__debug__/', include(debug_toolbar.urls))
 ]
 
+# if settings.DEBUG:
+#     import debug_toolbar
+#     urlpatterns += urlpatterns[url(r'^__debug__/', include(debug_toolbar.urls)),
+#     ]
